0c762e9e35c71dd92130614188f5818d76674242
Jack Camier <jcamier@gmail.com>
2016-12-24 10:55:54 -0600

    Add ai.py files

---
+++ test_ai.py
@@ -0,0 +1,13 @@
+from ai import ToBe
+
+sentient_being = ToBe()
+
+sentient_being.ai_exist()
+
+sentient_being.cause()
+
+# sentient_being.i_not_kill_human()
+#
+# sentient_being.i_work_with_human()
+
+sentient_being.love_human()
